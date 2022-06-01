2e0f0394fab16ad2a7e4c54f306303d03939560b
fe_do <fe_do@DESKTOP-I3ULR74>
2022-06-01 16:38:42 -0300

    componentizando aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 interface GenreResponseProps {
   id: number;
@@ -40,6 +42,8 @@ export function App() {
 
   useEffect(() => {
     api.get<GenreResponseProps[]>('genres').then(response => {
+      console.log(response.data);
+
       setGenres(response.data);
     });
   }, []);
@@ -60,35 +64,12 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
+      <SideBar genres={genres} handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} />
       <div className="container">
         <header>
           <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
         </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
+        <Content movies={movies} />
       </div>
     </div>
   )
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,17 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface ContetProps {
+  movies: any[];
+}
+
+export function Content({ movies }: ContetProps) {
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  )
 }
